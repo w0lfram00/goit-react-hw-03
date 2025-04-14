7c7a1653d30d33d10e8f006c19859abafd798a5b
Mikita <ghjhjr11@gmail.com>
2025-04-14 14:47:15 +0300

    3

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,6 @@
-import { Field, Form, Formik } from "formik";
-import React, { useState } from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import s from "./ContactForm.module.css";
+import * as Yup from "yup";
 
 const ContactForm = ({ addContact }) => {
   const initialValues = { name: "", number: "" };
@@ -8,17 +8,36 @@ const ContactForm = ({ addContact }) => {
     addContact({ name, number });
     actions.resetForm();
   };
+  const contactSchema = Yup.object().shape({
+    name: Yup.string().min(3).max(50).required(),
+    number: Yup.string()
+      .matches(
+        /^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$/,
+        "incorrect format"
+      )
+      .required(),
+  });
   return (
     <div>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={contactSchema}
+        onSubmit={handleSubmit}
+      >
         <Form className={s.form}>
           <label>
             <p>Name</p>
             <Field type="text" name="name" />
+            <span>
+              <ErrorMessage name="name" />
+            </span>
           </label>
           <label>
             <p>Number</p>
-            <Field type="text" name="number" />
+            <Field type="tel" name="number" />
+            <span>
+              <ErrorMessage name="number" />
+            </span>
           </label>
           <button type="submit">Create</button>
         </Form>
